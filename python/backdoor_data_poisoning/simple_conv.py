56e3c8726aed764cd9f47e296566ba3269e41503
Aref Asvadishirehjini <arefasvadi@gmail.com>
2019-02-01 11:41:19 -0600

    jupyter notebook added!

---
+++ python/backdoor_data_poisoning/simple_conv.py
@@ -0,0 +1,27 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import numpy as np
+import tensorflow as tf
+
+# Three convolutional layers/Relu with max pooling
+class SimpleConvModel(object):
+
+    def __init__(self,config):
+        self.conv_padding = config.conv_padding
+        self.num_convs = config.num_convs
+        self.num_fc = config.num_fc
+
+    def __build_conv():
+
+    def __build_fconn():
+
+    def __build_loss():
+
+    def __build_opt():
+
+    def __build_acc():
+
+    def model():
+    
